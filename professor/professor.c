8dad8df5f422cffab02e91ff529953d7a4c04b1f
adelsonsljunior <junioradelsonst@gmail.com>
2025-07-13 20:53:49 -0300

    Update professor data and improve code formatting

--- professor/professor.c
+++ professor/professor.c
@@ -3,23 +3,26 @@
 #include <string.h>
 #include "professor.h"
 
-// Dados fixos
+// Dados pre-definidos de professores
 Professor professoresFixos[MAX_PROFESSORES] = {
-    {"P01", "Ana Silva", "ana@uni.edu", "1234"},
-    {"P02", "Bruno Lima", "bruno@uni.edu", "1234"},
-    {"P03", "Carla Souza", "carla@uni.edu", "1234"}
-};
+    {"P01", "Cledja Rolim", "cledja@ifal.edu.br", "1234"},
+    {"P02", "Tarsis Marinho", "tarsis@ifal.edu.br", "1234"},
+    {"P03", "Fernando Antonio", "fernando@ifal.edu.br", "1234"}};
 
-void carregarProfessores(ProfNode** raiz) {
+void carregarProfessores(ProfNode **raiz)
+{
     *raiz = NULL;
-    for (int i = 0; i < MAX_PROFESSORES; i++) {
+    for (int i = 0; i < MAX_PROFESSORES; i++)
+    {
         *raiz = inserirProfessor(*raiz, professoresFixos[i]);
     }
 }
 
-ProfNode* inserirProfessor(ProfNode* raiz, Professor p) {
-    if (raiz == NULL) {
-        ProfNode* novo = (ProfNode*)malloc(sizeof(ProfNode));
+ProfNode *inserirProfessor(ProfNode *raiz, Professor p)
+{
+    if (raiz == NULL)
+    {
+        ProfNode *novo = (ProfNode *)malloc(sizeof(ProfNode));
         novo->prof = p;
         novo->esq = novo->dir = NULL;
         return novo;
@@ -31,16 +34,23 @@ ProfNode* inserirProfessor(ProfNode* raiz, Professor p) {
     return raiz;
 }
 
-ProfNode* buscarProfessor(ProfNode* raiz, const char* cod) {
-    if (raiz == NULL) return NULL;
+ProfNode *buscarProfessor(ProfNode *raiz, const char *cod)
+{
+    if (raiz == NULL)
+        return NULL;
     int cmp = strcmp(cod, raiz->prof.cod);
-    if (cmp == 0) return raiz;
-    else if (cmp < 0) return buscarProfessor(raiz->esq, cod);
-    else return buscarProfessor(raiz->dir, cod);
+    if (cmp == 0)
+        return raiz;
+    else if (cmp < 0)
+        return buscarProfessor(raiz->esq, cod);
+    else
+        return buscarProfessor(raiz->dir, cod);
 }
 
-void liberarArvoreProfessores(ProfNode* raiz) {
-    if (raiz == NULL) return;
+void liberarArvoreProfessores(ProfNode *raiz)
+{
+    if (raiz == NULL)
+        return;
     liberarArvoreProfessores(raiz->esq);
     liberarArvoreProfessores(raiz->dir);
     free(raiz);
