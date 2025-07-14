d6d2b117b761493d2d5fd5ee384c99111807462a
adelsonsljunior <junioradelsonst@gmail.com>
2025-07-13 21:18:07 -0300

    rename vars in buscaBinaria

--- busca/busca.c
+++ busca/busca.c
@@ -3,17 +3,17 @@
 
 int buscaBinaria(Sala salas[], int n, const char *numero)
 {
-    int esq = 0, dir = n - 1;
-    while (esq <= dir)
+    int inicio = 0, fim = n - 1;
+    while (inicio <= fim)
     {
-        int meio = (esq + dir) / 2;
+        int meio = (inicio + fim) / 2;
         int cmp = strcmp(numero, salas[meio].numero);
         if (cmp == 0)
             return meio;
         else if (cmp < 0)
-            dir = meio - 1;
+            fim = meio - 1;
         else
-            esq = meio + 1;
+            inicio = meio + 1;
     }
     return -1;
 }
